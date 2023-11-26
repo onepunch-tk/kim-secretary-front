edc77bbcbb3f7d2e6103a98c9ba38e5ddae8c49f
TaekyungHa <86tkstar@gmail.com>
2023-11-26 22:18:35 +0900

    home page ui

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,3 +1,38 @@
+import { useCalendar } from "@utils/store/calendar.ts";
+import { Fragment } from "react";
+
 export function Home() {
-  return <h1>home</h1>;
+  const { year, month, days, selectedCalendar } = useCalendar();
+  const changeCalendarHandle = (navDir: "prev" | "next") => {
+    const updateDate = {
+      year: Number(year),
+      month: 0,
+    };
+    if (navDir === "prev") {
+      updateDate.year =
+        Number(month) === 1 ? Number(year) - 1 : updateDate.year;
+      updateDate.month = Number(month) === 1 ? 11 : Number(month) - 2;
+      selectedCalendar(updateDate.year, updateDate.month);
+    } else {
+      updateDate.year =
+        Number(month) === 12 ? Number(year) + 1 : updateDate.year;
+      updateDate.month = Number(month) === 12 ? 0 : Number(month);
+      selectedCalendar(updateDate.year, updateDate.month);
+    }
+  };
+  return (
+    <Fragment>
+      <h1>{year}</h1>
+      <h2>{month}</h2>
+      {days.map((day) => (
+        <div key={day.dayNumber}>
+          <span>{day.dayNumber}</span>
+          <span>{day.dayName}</span>
+          {day.isHoliday ? <span>{day.name}</span> : null}
+        </div>
+      ))}
+      <button onClick={() => changeCalendarHandle("prev")}>prev</button>
+      <button onClick={() => changeCalendarHandle("next")}>next</button>
+    </Fragment>
+  );
 }
