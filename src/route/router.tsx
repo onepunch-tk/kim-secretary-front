7fa0c04f71669f42f1e659aa29bf7ff3c9e5c833
TaekyungHa <86tkstar@gmail.com>
2023-11-28 21:24:04 +0900

    add gh-pages and deploy

--- src/route/router.tsx
+++ src/route/router.tsx
@@ -2,9 +2,10 @@ import { createBrowserRouter } from "react-router-dom";
 import { Root } from "@/route/Root.tsx";
 import { Home } from "@views/Home.tsx";
 
+console.log(import.meta.env.PUBLIC_URL);
 export const router = createBrowserRouter([
   {
-    path: import.meta.env.PUBLIC_URL,
+    path: "https://onepunch-tk.github.io/kim-secretary-front",
     element: <Root />,
     children: [
       {
