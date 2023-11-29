3469e0bca5dac24740e6bd81b6f54eaaaddd021d
TaekyungHa <86tkstar@gmail.com>
2023-11-29 15:04:42 +0900

    add gh-pages and deploy

--- src/route/router.tsx
+++ src/route/router.tsx
@@ -2,16 +2,23 @@ import { createBrowserRouter } from "react-router-dom";
 import { Root } from "@/route/Root.tsx";
 import { Home } from "@views/Home.tsx";
 
-console.log(import.meta.env.PUBLIC_URL);
-export const router = createBrowserRouter([
+console.log();
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-    ],
-  },
-]);
+    ...(import.meta.env.VITE_DEPLOY_ROLE === "gh-pages" && {
+      basename: "/kim-secretary-front",
+    }),
+  }
+);
